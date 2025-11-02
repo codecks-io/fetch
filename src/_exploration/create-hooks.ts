ecf91a167868c90726ab2c4f018d0d7877d0e01f
Daniel Berndt <daniel.berndt@gmail.com>
2025-11-02 09:34:20 +0100

    exports expose exploration

---
+++ src/_exploration/create-hooks.ts
@@ -0,0 +1,37 @@
+import type {_rootDesc} from "../models/_root";
+import type {InferModelQuery, InferRelQuery, ModelQuery, RelQuery} from "../query-type";
+import {use, useSyncExternalStore} from "react";
+import type {Store} from "./store";
+import {UserQueryManager} from "./user-query-manager";
+
+export const createHooks = <ModelMap extends Record<string, any>>(store: Store) => {
+  const queryManager = new UserQueryManager(store);
+
+  const useQueryManager = (model: string, ids: string[], q: ModelQuery<any, any>) => {
+    // queryObj needs to be stable across renders if the query parts stay the same
+    const queryObj = queryManager.getQuery(model, ids, q);
+    const val = useSyncExternalStore<any>(queryObj.subscribe, queryObj.get);
+    if (val.state === "pending") return use(val.promise);
+    return val.value;
+  };
+
+  const useFetch = <
+    K extends keyof ModelMap,
+    Id extends string,
+    const Q extends ModelQuery<ModelMap[K], ModelMap>,
+  >(
+    model: K,
+    id: Id,
+    q: Q
+  ): InferModelQuery<ModelMap[K], Q, ModelMap> => {
+    return useQueryManager(model as string, [id], q);
+  };
+
+  const useFetchFromRoot = <const Q extends RelQuery<typeof _rootDesc, ModelMap>>(
+    q: Q
+  ): InferRelQuery<typeof _rootDesc, Q, ModelMap> => {
+    return useQueryManager("_root", [""], {relations: q});
+  };
+
+  return {useFetch, useFetchFromRoot};
+};
--- src/_exploration/react-hooks.ts
+++ src/_exploration/react-hooks.ts
@@ -1,11 +1,8 @@
 import {modelMap} from "../models";
-import type {_rootDesc} from "../models/_root";
-import type {InferModelQuery, InferRelQuery, ModelQuery, RelQuery} from "../query-type";
-import {use, useSyncExternalStore} from "react";
 import {Store} from "./store";
-import {UserQueryManager} from "./user-query-manager";
 import {BatchedLoader} from "./loader";
 import {ApiRequester} from "./api-requester";
+import {createHooks} from "./create-hooks";
 
 type ModelMap = typeof modelMap;
 
@@ -17,30 +14,5 @@ const loader = new BatchedLoader({
 });
 
 const store = new Store(modelMap, loader);
-const queryManager = new UserQueryManager(store);
 
-const useQueryManager = (model: string, ids: string[], q: ModelQuery<any, any>) => {
-  // queryObj needs to be stable accross renders if the query parts stay the same
-  const queryObj = queryManager.getQuery(model, ids, q);
-  const val = useSyncExternalStore<any>(queryObj.subscribe, queryObj.get);
-  if (val.state === "pending") return use(val.promise);
-  return val.value;
-};
-
-export const useFetch = <
-  K extends keyof ModelMap,
-  Id extends string,
-  const Q extends ModelQuery<ModelMap[K], ModelMap>,
->(
-  model: K,
-  id: Id,
-  q: Q
-): InferModelQuery<ModelMap[K], Q, ModelMap> => {
-  return useQueryManager(model, [id], q);
-};
-
-export const useFetchFromRoot = <const Q extends RelQuery<typeof _rootDesc, ModelMap>>(
-  q: Q
-): InferRelQuery<typeof _rootDesc, Q, ModelMap> => {
-  return useQueryManager("_root", [""], {relations: q});
-};
+export const {useFetch, useFetchFromRoot} = createHooks<ModelMap>(store);
